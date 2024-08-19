a5dcbfbea875bf2f57cf9bafcd76c733ad571582
NeverTold <149240019+NeverTold@users.noreply.github.com>
2024-08-19 11:31:16 +0700

    feature: reverse function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
 module.exports = function reverse (n) {
-  
+  var int = Sting(n);
+  var result = "";
+  for (i=0; i < int.length; i++){
+    result = int[i]+result;
+  }
+  return Number(result);
 }
